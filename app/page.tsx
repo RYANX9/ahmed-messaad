b4cafdc3d91e2075d447d59166ecfec8fa053782
Ahmed Messaad <78614946+RYANX9@users.noreply.github.com>
2025-10-14 21:38:58 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
  
 import React, { useState } from "react";
-import Image from "next/image";
+import Image from "next/image"; 
 
 export default function Page() {
   const [activeProject, setActiveProject] = useState<string | null>("airm");
